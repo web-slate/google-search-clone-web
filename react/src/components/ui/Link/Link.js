415c914f5659c48c508c66a786b67139cad47700
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2022-03-04 19:57:24 +0530

    feat(ui/Link): link component with variants

--- react/src/components/ui/Link/Link.js
+++ react/src/components/ui/Link/Link.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import styles from './Link.module.css'
 
-function Link() {
+function Link(props) {
+  const { variant, ...anchorProps } = props
+
   return (
-    <>
-      <p className={styles.line}>Link goes here.</p>
-    </>
+    <a className={`${styles.link} ${styles[variant]}`} {...anchorProps}>
+      {props.title}
+    </a>
   )
+}
+
+Link.propTypes = {
+  title: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf(['primary', 'dark']),
+}
+
+Link.defaultProps = {
+  variant: 'primary',
 }
 
 export default Link
