bb1aade552c16f9743293d6d1c71cf26979c6c0d
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2022-03-04 18:17:12 +0530

    feat(ui/Button): component with variants

--- react/src/components/ui/buttons/Button/Button.js
+++ react/src/components/ui/buttons/Button/Button.js
@@ -1,12 +1,27 @@
 import React from 'react'
+
+import PropTypes from 'prop-types'
+
 import styles from './Button.module.css'
 
-function Button() {
+function Button(props) {
+  const { title, variant, onClick } = props
+
   return (
-    <>
-      <p className={styles.line}>Button goes here.</p>
-    </>
+    <button className={`${styles.button} ${styles[variant]}`} onClick={onClick}>
+      {title}
+    </button>
   )
+}
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  variant: PropTypes.oneOf(['primary', 'light']),
+}
+
+Button.defaultProps = {
+  variant: 'light',
 }
 
 export default Button
--- react/src/components/ui/buttons/IconButton/index.js
+++ react/src/components/ui/buttons/IconButton/index.js
@@ -1,1 +1,1 @@
-export { default } from './IconButton'
+export { default as IconButton } from './IconButton'
