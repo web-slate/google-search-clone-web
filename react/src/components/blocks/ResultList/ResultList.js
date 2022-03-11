c0e3822afcfcc512be92116e2ee8859b524ad4eb
sriramanvellingiri <sriramanvellingiri@gmail.com>
2022-03-11 20:35:04 +0530

    feat(ResultNotFound): Adding Result Not found page

--- react/src/components/blocks/ResultList/ResultList.js
+++ react/src/components/blocks/ResultList/ResultList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './ResultList.module.css'
+import {ResultNotFound} from './ResultNotFound'
 
 function ResultList() {
   return (
     <>
       <p className={styles.line}>ResultList goes here.</p>
+      <ResultNotFound />
     </>
   )
 }
