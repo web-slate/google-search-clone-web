bbb9afa39d3e2ca1940c82759df5266949c2e943
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2022-03-12 15:22:03 +0530

    feat(block/ResultList): add list item ui

--- react/src/modules/SearchResults/SearchResults.js
+++ react/src/modules/SearchResults/SearchResults.js
@@ -2,15 +2,24 @@ import React from 'react'
 
 // blocks
 import SearchForm from '@/blocks/SearchForm'
+import ResultList from '@/blocks/ResultList'
 
 // styles
 import styles from './SearchResults.module.css'
 
 function SearchResults() {
   return (
-    <div className={styles.container}>
+    <>
       <SearchForm />
-    </div>
+      <div className={styles.container}>
+        <div className={styles.resultPage}>
+          <div className={styles.resultCount}>
+            About 14,40,00,000 results<nobr> (0.61 seconds)&nbsp;</nobr>
+          </div>
+          <ResultList />
+        </div>
+      </div>
+    </>
   )
 }
 
