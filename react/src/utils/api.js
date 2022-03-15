b001b2b0b9e298e4dbb94ee5884c9e59c1b17213
jpukg <jpukg@users.noreply.github.com>
2022-03-15 19:30:14 +0100

    fix(backend-url): Fix backend URL

--- react/src/utils/api.js
+++ react/src/utils/api.js
@@ -1,2 +1,2 @@
-//export const API_URL = 'https://google-search-app-api.herokuapp.com'
-export const API_URL = 'http://193.121.74.194:8080'
+export const API_URL = 'https://google-backend-api.herokuapp.com'
+// export const API_URL = 'http://193.121.74.194:8080'
