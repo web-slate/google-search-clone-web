b7e8c8f9899f686f2687801191879a4058774565
govindarajkp <govindaraj.k.p@gmail.com>
2022-03-18 18:19:33 +0530

    Revert "tech(utils/base): adding bootstrap css"
    This reverts commit fc3979d78d47a64131f40f5a01b7feae49296ecc.

--- react/src/index.js
+++ react/src/index.js
@@ -4,6 +4,5 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 import './style.css'
-import './bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('google-search-clone-web'))
