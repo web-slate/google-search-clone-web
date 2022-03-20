47115f08842b239eb4886a5add22e7086a397dd0
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2022-03-20 11:31:43 +0530

    fix(module/SearchResults): add search on button press

--- react/src/components/blocks/ResultNotFound/ResultNotFound.js
+++ react/src/components/blocks/ResultNotFound/ResultNotFound.js
@@ -7,19 +7,26 @@ import { useI18n } from '@/i18n'
 import NoResultFoundImg from '../../../static/images/result_not_found.svg'
 
 function ResultNotFound(props) {
-
   const { searchText = '' } = props
   const { formatMessage } = useI18n()
 
   return (
-    <>
-    <div class={styles.container}>
-      <h2>{formatMessage({ id: 'yoursearch' })} - {searchText} - {formatMessage({ id: 'didnotmatchanydocuments' })}.</h2>
+    <div className={styles.container}>
+      <h2>
+        {formatMessage({ id: 'yoursearch' })} - {searchText} -{' '}
+        {formatMessage({ id: 'didnotmatchanydocuments' })}.
+      </h2>
       <h3>{formatMessage({ id: 'suggestions' })} :</h3>
       <ul>
-            <li> {formatMessage({ id: 'make_sure_that_all_words_are_spelled_correctly' })}.</li>
-            <li> {formatMessage({ id: 'try_different_keywords' })}. </li>
-            <li> {formatMessage({ id: 'try_more_general_keywords' })}.</li>
+        <li>
+          {' '}
+          {formatMessage({
+            id: 'make_sure_that_all_words_are_spelled_correctly',
+          })}
+          .
+        </li>
+        <li> {formatMessage({ id: 'try_different_keywords' })}. </li>
+        <li> {formatMessage({ id: 'try_more_general_keywords' })}.</li>
       </ul>
       <img
         src={NoResultFoundImg}
@@ -27,8 +34,7 @@ function ResultNotFound(props) {
         height="500"
         width="600"
       />
-      </div>
-    </>
+    </div>
   )
 }
 
